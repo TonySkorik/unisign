a1833aa2a1636e3eeda64f9fb17e8a2a467b6e94
TS <just.skorik@gmail.com>
2016-07-27 19:29:31 +0300

    Basic design Folder structure corresponding with MVVM Interop && XML format documented + direct HTML for WebBrowser dependency property

--- Signer/MainWindow.xaml.cs
+++ Signer/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Signer.ViewModel;
+using Signer.Model;
+
 namespace Signer {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
--- Signer/ViewModel/MainViewModel.cs
+++ Signer/ViewModel/MainViewModel.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Signer {
+namespace Signer.ViewModel {
 	class MainViewModel:INotifyPropertyChanged {
 
 		public event PropertyChangedEventHandler PropertyChanged;
